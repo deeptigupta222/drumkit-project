0756985fc9d0323ae825723338f15281ecfcaf79
deeptigupta222 <deeptig22@nagurukul.org>
2023-07-12 12:52:54 +0530

    intial commiy

--- script.js
+++ script.js
@@ -14,32 +14,32 @@ for (let i = 0; i < arrayOfDrums.length; i++) {
 
 const makeSound = (key) => {
   switch (key) {
-    case "w":
+    case "D":
       let tom1 = new Audio("sounds/tom-4.mp3");
       tom1.play();
       break;
-    case "a":
-      let tom2 = new Audio("sounds/tom-2.mp3");
+    case "r":
+      let tom2 = new Audio("sounds/crash.mp3");
       tom2.play();
       break;
-    case "s":
-      let tom3 = new Audio("sounds/tom-3.mp3");
+    case "u":
+      let tom3 = new Audio("sounds/kick-bass.mp3");
       tom3.play();
       break;
-    case "d":
+    case "m":
       let tom4 = new Audio("sounds/tom-1.mp3");
       tom4.play();
       break;
-    case "j":
+    case "K":
       let snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
-    case "k":
-      let crash = new Audio("sounds/crash.mp3");
+    case "i":
+      let crash = new Audio("sounds/tom-2.mp3");
       crash.play();
       break;
-    case "l":
-      let kick = new Audio("sounds/kick-bass.mp3");
+    case "t":
+      let kick = new Audio("sounds/tom-3.mp3");
       kick.play();
       break;
     default:
